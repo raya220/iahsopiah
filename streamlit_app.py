586ade66bff447e19cd335451e58b9ab00aced78
raya220 <rayarabbani220@gmail.com>
2025-05-28 07:50:23 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("nama saya iah Sopiah")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "dari majelis taklim albarokah"
 )
